4390b8aa1d1665fca22db8d56d7ad180b15f140d
skyhawk <skyhawk@yandex-team.ru>
2014-08-04 12:32:50 +0400

    some improvements

--- learner.py
+++ learner.py
@@ -1,91 +1,88 @@
 # -*- coding: utf8 -*-
 
+import sys
 from random import randint, random, shuffle
+sys.path.insert(0, 'util')
+import argparse
 
-def found(input, cur):
-    for word in cur[1:len(cur)]:
-        if input == word:
-            return True
-    return False
+def weightedChoise(weights):
+    weights = [w / sum(weights) for w in weights]
+    rand_value = random()
 
-def randomLearner(n, range1, range2):
-    priorities = [1.0]*n
+    s = 0.0
+    for i in range(len(weights)):
+        s += weights[i]
+        if s >= rand_value:
+            return i
+    return len(weights) - 1
+
+def randomLearner(vocabulary):
+    priorities = [1.0] * len(vocabulary)
     while True:
-        priorities = [p/sum(priorities) for p in priorities]
-        rand = random()
-        cur_numb = 0
-        iterator = priorities[0]
-        while iterator <= rand:
-            cur_numb += 1
-            iterator += priorities[cur_numb]
-        cur_numb = min(cur_numb, n-1)
-        cur = f[range1 + cur_numb].split()
-        incorrect = checkOneWord(cur)
-        if incorrect == 0:
-            priorities[cur_numb] /= n
-        else:
-            priorities[cur_numb] *= n*incorrect
+        priorities = [p / sum(priorities) for p in priorities]
+        choise = weightedChoise(priorities)
+        word = vocabulary[choise]
 
-def checkOneWord(cur):
-    correct = 0
+        incorrect = checkOneWord(word)
+        if incorrect == 0:
+            priorities[choise] /= len(vocabulary)
+        else:
+            priorities[choise] *= len(vocabulary) * incorrect
+
+def checkOneWord(word):
     incorrect = 0
-    while correct == 0:
-        input = raw_input(cur[0])
+    while True:
+        input = raw_input(word[0] + ' ')
+
         if input == "q":
             exit()
-        if input == "show":
-            stroka = ""
-            for word in cur:
-                stroka += " " + word
-            print stroka
+        if input == "s" or input == "":
+            print " ".join(word)
+            incorrect += 1
             continue
-        if found(input, cur):
+        if input in word:
             print "correct!"
-            correct = 1
+            break
         else:
             print "incorrect, please try again!"
             incorrect += 1
     return incorrect
 
-def allRepeater(n, range1, range2):
-    sequence = range(range1, range2 + 1)
+def allRepeater(words):
+    sequence = range(len(words))
     shuffle(sequence)
     incorrect = 0
-    for i in range(range1, range2 + 1):
+    for i in range(len(words)):
         print str(i) + ": "
-        cur =f[sequence[i]].split()
-        if checkOneWord(cur) != 0:
+        choise = words[sequence[i]]
+        if checkOneWord(choise) != 0:
             incorrect += 1
-    print "You have (out of " + str(n) + "):"
+    print "You have (out of " + str(len(words)) + "):"
     print str(incorrect) + " - incorrect"
-    print str(n - incorrect) + " - correct"
+    print str(len(words) - incorrect) + " - correct"
 
-def Cycle(n):
-    print "Hello! There is " + str(n) + " words in vocabulary"
-    r = raw_input("Wanna choose range?(y/n)")
-    if r == "y":
-        range1 = int(raw_input("please, choose left range")) - 1
-        range2 = int(raw_input("please, choose right range")) - 1
-    elif r == "n":
-        range1 = 0
-        range2 = n-1
-    elif r == "q":
-        quit()
+if __name__ == '__main__':
+    argument_parser = argparse.ArgumentParser(add_help=True)
+    argument_parser.add_argument('-v', '--vocabulary', action='store', required=True,
+        metavar='VOCABULARY_FILE', dest='vocabulary_path', help='file with vocabulary')
+    argument_parser.add_argument('-r', '--range', action='store', required=False, nargs=2,
+        metavar=('LEFT', 'RIGHT'), dest='range', type=int, help='range of vocubulary to learn')
+    argument_parser.add_argument('-m', '--mode', action='store', required=True,
+        metavar=('MODE'), dest='mode', help='program mode: r (repeater) or l (learner)')
+    arguments = argument_parser.parse_args()
+
+    vocabulary = [v.split() for v in open(arguments.vocabulary_path, "r").readlines()]
+    
+    # Detecting range
+    if arguments.range:
+        if (arguments.range[0] < 0 or arguments.range[1] > len(vocabulary) or arguments.range[0] > arguments.range[1]):
+            raise RuntimeError("Bad range values: %d %d for %d words in dictionary" %
+                (arguments.range[0], arguments.range[1], len(vocabulary)))
+        vocabulary = vocabulary[arguments.range[0] : arguments.range[1] + 1]
+
+    if arguments.mode == "l":
+        randomLearner(vocabulary)
+    elif arguments.mode == "r":
+        allRepeater(vocabulary)
     else:
-        Cycle(n)
-    n1 = range2 - range1 + 1
-    choice = raw_input("please, input mode:\nl - learner\nr - repeater\n")
-    if choice == "l":
-        randomLearner(n1, range1, range2)
-    elif choice == "r":
-        allRepeater(n1, range1, range2)
-        Cycle(n)
-    elif choice == "q":
-        exit()
-    else:
-        Cycle(n)
-
-f = open("vocabulary.txt", "r").readlines()
-n = len(f)
-Cycle(n)
-
+        raise RuntimeError("Bad program mode: " + arguments.mode)
